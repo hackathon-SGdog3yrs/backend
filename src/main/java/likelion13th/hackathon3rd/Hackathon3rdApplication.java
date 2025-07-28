5c5874d4723cccf5313384e06aaf28187bdce38d
Sua Jun <mjun3712@gmail.com>
2025-07-29 00:24:08 +0900

    initial commit

---
+++ src/main/java/likelion13th/hackathon3rd/Hackathon3rdApplication.java
@@ -0,0 +1,13 @@
+package likelion13th.hackathon3rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hackathon3rdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hackathon3rdApplication.class, args);
+	}
+
+}
