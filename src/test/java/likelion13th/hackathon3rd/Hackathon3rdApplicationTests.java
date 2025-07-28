31e6f2070862126e077be68ceb7fe0b984659360
Sua Jun <mjun3712@gmail.com>
2025-07-29 00:24:08 +0900

    initial commit

---
+++ src/test/java/likelion13th/hackathon3rd/Hackathon3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion13th.hackathon3rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hackathon3rdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
